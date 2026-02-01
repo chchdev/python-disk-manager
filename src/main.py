6bf4f8109fddd2cb20b9cce0f584b832f374a792
Ben <ben@goliath.net.nz>
2026-02-01 17:48:09 +1300

    Added a sleep function to prevent spam

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 import os
+import time
 
 def main():
     # Needs to be runnning 24-7
     while True:
         try:
             os.system("python src/space.py")
+            
+            # Run every 30 minutes
+            time.sleep(1800)
         except Exception as e:
             print(f"An error occurred: {e}")
 
