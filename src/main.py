951150524c7b2a1be52ea5e02f9d80baeaae1b47
Ben <ben@goliath.net.nz>
2026-02-01 19:59:40 +1300

    Set to run every 3 hours

--- src/main.py
+++ src/main.py
@@ -12,8 +12,8 @@ def main():
             script_path = project_root / "src" / "space.py"
             subprocess.run([sys.executable, str(script_path)], check=True)
             
-            # Run every 30 minutes
-            time.sleep(1800)
+            # Run every 3 hours
+            time.sleep(3 * 60 * 60)
         except Exception as e:
             print(f"An error occurred: {e}")
 
